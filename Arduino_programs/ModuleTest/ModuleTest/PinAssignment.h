5f06acadd5a496d3d070713a1dfe4e1baac0f59e
dorayaki4369 <dorayaki4369@outlook.jp>
2016-12-10 21:04:35 +0900

    ピン番号を変更

--- Arduino_programs/ModuleTest/ModuleTest/PinAssignment.h
+++ Arduino_programs/ModuleTest/ModuleTest/PinAssignment.h
@@ -11,48 +11,45 @@
 
 //TODO:適切なピン名に変更する
 //MOTOR
-#define MTR_1ENA 26 //Dingtal
-#define MTR_1ENB 27
-#define MTR_2ENA 29
-#define MTR_2ENB 30
-//#define MTR_A3 28
-//#define MTR_B3 31
-#define P_N1 2  //PWM
-#define P_N2 3
-#define P_E1 4
-#define P_E2 5
-#define P_S1 6
-#define P_S2 7
-#define P_W1 8
-#define P_W2 9
+#define MTR_1ENA 4 //PWM
+#define MTR_1ENB 5
+#define MTR_2ENA 6
+#define MTR_2ENB 7
+#define MTR_N1 33  //Digital
+#define MTR_N2 34
+#define MTR_E1 35
+#define MTR_E2 36
+#define MTR_S1 37
+#define MTR_S2 38
+#define MTR_W1 39
+#define MTR_W2 40
 
 //ENCODER
-#define ENC_N1 32
-#define ENC_N2 33
-#define ENC_E1 34
-#define ENC_E2 35
-#define ENC_S1 36
-#define ENC_S2 37
-#define ENC_W1 38
-#define ENC_W2 39
+#define ENC_N1 44
+#define ENC_N2 45
+#define ENC_E1 46
+#define ENC_E2 47
+#define ENC_S1 48
+#define ENC_S2 49
+#define ENC_W1 50
+#define ENC_W2 51
 
 //USS
-#define USS_N 40 //Dingtal
-#define USS_E 41
-#define USS_S 42
-#define USS_W 43
+#define USS_N 25 //Dingtal
+#define USS_E 26
+#define USS_S 27
+#define USS_W 28
 
-//INFRARED
-#define IR 22 //CS
+//IR_A,IR_B
+#define IR_CS 23
+#define IR_CLK 42
 
 //TAPELED
-#define TL_0 44 //Digital
-#define TL_1 45
-#define TL_2 46
+#define TL_0 29 //Digital
+#define TL_1 30
+#define TL_2 32
 
 //MP3_A,MP3_B
-#define MP3_1 23 //CS
-#define MP3_2 24
-#define MP3_3 25
+#define MP3_CS 24
 
 #endif
