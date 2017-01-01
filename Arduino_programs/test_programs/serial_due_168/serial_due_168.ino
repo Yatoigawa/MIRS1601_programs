707897410a06f31fc66dd6452285a4013d3799fe
Yatoigawa <flandrescarlet823@gmail.com>
2017-01-01 23:33:39 +0900

    USARTテストプログラムの完成
    確認済み

--- Arduino_programs/test_programs/serial_due_168/serial_due_168.ino
+++ Arduino_programs/test_programs/serial_due_168/serial_due_168.ino
@@ -1,34 +1,44 @@
 #include <Scheduler.h>
 
+typedef union {
+	uint16_t val;
+	struct {
+		uint8_t lsb;
+		uint8_t msb;
+	};
+} infrared_data_t;
+
 const int BAUDRATE = 115200;
-uint8_t serial_data = 0x00;
+
+infrared_data_t data;
+int i = 0;
 
 void setup() {
 	Serial.begin(BAUDRATE);
 	Serial2.begin(BAUDRATE);
-
-	// バッファクリア
-	while(Serial.available() > 0){
-		Serial.read();
-	}
-	while(Serial2.available() > 0){
-		Serial2.read();
-	}
 	Scheduler.startLoop( serial_task );
 }
 
-
 void loop(){
-	if(serial_data > 0){
-		Serial.println(serial_data, HEX);
+	Serial2.write(i + 0x80);
+	if(data.val > 0){
+		Serial.print("count: ");
+		Serial.print(i);
+		Serial.print(" data: ");
+		Serial.println(data.val, HEX);
 	}
+	i++;
 	delay(500);
 }
 
 void serial_task(){
-	if(Serial2.available() > 0){
-		serial_data = Serial2.read();
+	if(Serial2.available() > 2){
+		if(Serial2.read() == 0xA4){
+			data.msb = (Serial2.read() /* & 0x0F */);
+			data.lsb =  Serial2.read();
+		}
 	}
+	delay(100);
 	yield();
 }
 
