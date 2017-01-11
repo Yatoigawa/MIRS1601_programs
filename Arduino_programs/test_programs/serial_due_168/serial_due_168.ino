25b6e9eed2b5f91434680f06de5d735197b7bbe4
Yatoigawa <flandrescarlet823@gmail.com>
2017-01-11 21:01:01 +0900

    インデントの調整
    自動整形に合わせるようにした

--- Arduino_programs/test_programs/serial_due_168/serial_due_168.ino
+++ Arduino_programs/test_programs/serial_due_168/serial_due_168.ino
@@ -1,11 +1,11 @@
 #include <Scheduler.h>
 
 typedef union {
-	uint16_t val;
-	struct {
-		uint8_t lsb;
-		uint8_t msb;
-	};
+  uint16_t val;
+  struct {
+    uint8_t lsb;
+    uint8_t msb;
+  };
 } infrared_data_t;
 
 const int BAUDRATE = 115200;
@@ -14,31 +14,34 @@ infrared_data_t data;
 int i = 0;
 
 void setup() {
-	Serial.begin(BAUDRATE);
-	Serial2.begin(BAUDRATE);
-	Scheduler.startLoop( serial_task );
+  Serial.begin(BAUDRATE);
+  Serial2.begin(BAUDRATE);
+  Scheduler.startLoop( serial_task );
 }
 
-void loop(){
-	Serial2.write(i + 0x80);
-	if(data.val > 0){
-		Serial.print("count: ");
-		Serial.print(i);
-		Serial.print(" data: ");
-		Serial.println(data.val, HEX);
-	}
-	i++;
-	delay(500);
+void loop() {
+  Serial2.write(i + 0x80);
+  if (data.val > 0) {
+    Serial.print("count: ");
+    Serial.print(i);
+    Serial.print(" data: ");
+    Serial.println(data.val, HEX);
+  }
+  i++;
+  if (i > 7) {
+    i = 0;
+  }
+  delay(500);
 }
 
-void serial_task(){
-	if(Serial2.available() > 2){
-		if(Serial2.read() == 0xA4){
-			data.msb = (Serial2.read() /* & 0x0F */);
-			data.lsb =  Serial2.read();
-		}
-	}
-	delay(100);
-	yield();
+void serial_task() {
+  if (Serial2.available() > 2) {
+    if (Serial2.read() == 0xA4) {
+      data.msb = (Serial2.read() /* & 0x0F */);
+      data.lsb =  Serial2.read();
+    }
+  }
+  delay(100);
+  yield();
 }
 
