26007c2cecb424719f0576342c665682472cd0bd
Yatoigawa <flandrescarlet823@gmail.com>
2017-01-13 23:20:59 +0900

    赤外線処理用の関数の記述

--- Arduino_programs/mg4_arduino_ver1601/infrared.ino
+++ Arduino_programs/mg4_arduino_ver1601/infrared.ino
@@ -1,9 +1,23 @@
+static infrared_data_t infrared_data;
+
 void infrared_open(void) {
-	Serial2.begin(BAUDRATE);
+	Serial2.begin(115200);
 
 	// バッファクリア
 	while(Serial2.available() > 0){
 		Serial2.read();
 	}
+}
+
+void infrared_serial_task(void){
+  int i = 0;
+  Serial2.write(i + 0x80);
+  while(!(Serial2.available() > 2));
+  if(Serial2.read() == 0xA4){
+    infrared_data.msb = (Serial2.read() & 0x0F);
+    infrared_data.lsb =  Serial2.read();
+  }
+  delay(10);
+  yield();
 }
 
