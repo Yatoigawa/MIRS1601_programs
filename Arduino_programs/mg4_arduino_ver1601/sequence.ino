ac7084383e96729860ee451ddbb93d0817b0e54f
Yatoigawa <flandrescarlet823@gmail.com>
2017-01-24 16:07:45 +0900

    sequence.inoを追加
    たぶんうごく

---
+++ Arduino_programs/mg4_arduino_ver1601/sequence.ino
@@ -0,0 +1,64 @@
+void sequence_main(void) {
+  char c = 0;
+  int ret;
+start:     // スタート初期
+  vel_ctrl_reset();
+
+  while ( ! ( ( ret = cpu_read(&c) ) == 0 ) );
+
+  switch (c) {
+    case 'l':
+      omuni_ctrl_set(OMN_SDE, 100, 40);
+      while (1) {
+        omuni_ctrl_execute();
+        if (cpu_com_read() == 's') {
+          goto start;
+        }
+        if (omuni_end_flag == 1) {
+          break;
+        }
+        delay(10);
+      }
+      omuni_ctrl_set(OMN_WDS, 100, 50);
+      while (1) {
+        omuni_ctrl_execute();
+        if (cpu_com_read() == 's') {
+          goto start;
+        }
+        if (omuni_end_flag == 1) {
+          break;
+        }
+        delay(10);
+      }
+      cpu_num_trans(1);
+      goto start;
+      break;
+    
+    case 'r':
+      omuni_ctrl_set(OMN_SDW, 100, 40);
+      while (1) {
+        omuni_ctrl_execute();
+        if (cpu_com_read() == 's') {
+          goto start;
+        }
+        if (omuni_end_flag == 1) {
+          break;
+        }
+        delay(10);
+      }
+      omuni_ctrl_set(OMN_EDS, 100, 50);
+      while (1) {
+        omuni_ctrl_execute();
+        if (cpu_com_read() == 's') {
+          goto start;
+        }
+        if (omuni_end_flag == 1) {
+          break;
+        }
+        delay(10);
+      }
+      cpu_num_trans(2);
+      goto start;
+      break;
+  }
+}
